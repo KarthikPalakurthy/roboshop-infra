f3d8d2714fd9101955c6f2759ec24606ff7184f6
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-04-28 06:55:43 +0100

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+module "network" {
+  source = "github.com/KarthikPalakurthy/tf-vpc-module"
+
+  for_each = var.vpc
+  cidr_block= each.value.cidr_block
 }
+
