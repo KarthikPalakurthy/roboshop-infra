d42aa87c26ba4d5f8e418b0531d0e74bea3d9df7
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-03 16:23:59 +0100

    terraform

--- main.tf
+++ main.tf
@@ -7,5 +7,6 @@ module "network" {
   cidr_block = each.value.cidr_block
   public_subnet_cidr= each.value.public_subnet_cidr
   private_subnet_cidr= each.value.private_subnet_cidr
+  availability_zones= each.value.availability_zones
 }
 
