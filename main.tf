7342ef4cad0c38b137286f7cb8289ace4378c787
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-03 11:28:44 +0100

    terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "network" {
   default_vpc_id = var.default_vpc_id
 
   for_each = var.vpc
+  cidr_block = each.value.cidr_block
   public_subnet_cidr= each.value.public_subnet_cidr
   private_subnet_cidr= each.value.private_subnet_cidr
 }
