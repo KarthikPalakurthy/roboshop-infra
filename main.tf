4aac551849b6c78328cb904542a39f98aaa2b51f
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-06 10:59:52 +0100

    terraform

--- main.tf
+++ main.tf
@@ -8,13 +8,20 @@ module "vpc" {
 
 }
 
-module "subnets" {
-  source = "github.com/KarthikPalakurthy/tf-subnet-module"
-  env=var.env
-  default_vpc_id = var.default_vpc_id
-  vpc_id = module.vpc.vpc_id
-  for_each = var.subnets
-  cidr_block = each.value.cidr_block
-  availability_zone = each.value.availability_zone
-  name = each.value.name
+#module "subnets" {
+#  source = "github.com/KarthikPalakurthy/tf-subnet-module"
+#  env=var.env
+#  default_vpc_id = var.default_vpc_id
+#  vpc_id = lookup(module.vpc,each.value.vpc_name,null )
+#
+#  vpc_id = module.vpc.vpc_id
+#  for_each = var.subnets
+#  cidr_block = each.value.cidr_block
+#  availability_zone = each.value.availability_zone
+#  name = each.value.name
+#}
+
+output "vpc_id" {
+  value =   vpc_id = lookup(module.vpc,each.value.vpc_name,null )
+
 }
