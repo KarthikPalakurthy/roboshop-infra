2dfb05586c5613d6ad98cd8b4c154d91a5644806
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-07 14:45:06 +0100

    terraform

--- main.tf
+++ main.tf
@@ -10,19 +10,9 @@ module "vpc" {
   availability_zone = each.value.availability_zone
 }
 
-#module "subnets" {
-#  source = "github.com/KarthikPalakurthy/tf-subnet-module"
-#  env=var.env
-#  default_vpc_id = var.default_vpc_id
-#  vpc_id = lookup(lookup(module.vpc,"main",null ),"vpc_id",null )
-#
-#  for_each = var.subnets
-#  cidr_block = each.value.cidr_block
-#  availability_zone = each.value.availability_zone
-#  name = each.value.name
-#  vpc_peering_connection_id = lookup(lookup(module.vpc,"main",null ),"vpc_peering_connection_id",null )
-#  internet_gw = lookup(each.value,"internet_gw",false )
-#  nat_gw = lookup(each.value, "nat_gw", false)
-#  internet_gw_id = lookup(lookup(module.vpc,"main",null ),"internet_gw_id",null )
-#}
-#
+module "docdb" {
+  source = "github.com/KarthikPalakurthy/tf-docdb-module"
+  env = var.env
+  default_vpc_id = var.default_vpc_id
+
+}
