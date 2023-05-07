adf399d194011190c4ba17bea099832545890236
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-07 10:11:40 +0100

    terraform

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ module "vpc" {
   source = "github.com/KarthikPalakurthy/tf-vpc-module"
   env=var.env
   default_vpc_id = var.default_vpc_id
+
   for_each = var.vpc
   cidr_block = each.value.cidr_block
   public_subnets = each.value.public_subnets
