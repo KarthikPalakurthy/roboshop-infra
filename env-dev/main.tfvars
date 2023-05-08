a69c996bf8f8afd4078d90bea7b58472c234faa1
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-08 06:11:38 +0100

    terraform

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -37,5 +37,6 @@ vpc = {
 docdb = {
   main = {
     vpc_name = "main"
+    subnet_name = "db"
   }
 }
--- main.tf
+++ main.tf
@@ -15,7 +15,11 @@ module "docdb" {
   env = var.env
 
   for_each = var.docdb
-  subnet_ids = lookup(lookup(lookup(module.vpc, each.value.vpc_name , null), each.value.subnets_name, null), "subnet_ids", null)
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name , null), "private_subnet_ids", null), each.value.subnet_name, null), "subnet_ids", null )
   allow_cidr_blocks = lookup(lookup(lookup(lookup(var.vpc , each.value.vpc_name , null), "private_subnets" , null),"app", null), "cidr_block", null)
   vpc_id = lookup(lookup(module.vpc , each.value.vpc_name , null), "vpc_id" , null)
+}
+
+output "vpc" {
+  value = module.vpc
 }
