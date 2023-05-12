b85f74953c107deb1824d29c70f9b6dd981c88b3
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-12 16:31:45 +0100

    terraform

--- main.tf
+++ main.tf
@@ -91,8 +91,6 @@ module "apps" {
   max_size = each.value.max_size
   min_size = each.value.min_size
   instance_type = each.value.instance_type
+  bastion_cidr = value.bastion_cidr
 }
 
-output "vpc" {
-  value = module.vpc
-}
