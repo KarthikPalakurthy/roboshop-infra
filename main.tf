9b2955dd9c2907465c186881331aee7532b1cd7b
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-20 16:25:00 +0100

    terraform

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ module "apps" {
   depends_on = [module.docdb, module.rds, module.alb, module.rabbitmq]
   for_each = var.apps
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name , null), each.value.subnet_type, null), each.value.subnet_name, null), "subnet_ids", null )
-  alb = lookup(lookup(module.alb,each.value.alb,null ), "dns_name", null )
+#  alb = lookup(lookup(module.alb,each.value.alb,null ), "dns_name", null )
   allow_cidr_blocks = lookup(lookup(lookup(lookup(var.vpc , each.value.vpc_name , null), each.value.allow_cidr_subnets_type , null),each.value.allow_cidr_subnets_name, null), "cidr_block", null)
   vpc_id = lookup(lookup(module.vpc , each.value.vpc_name , null), "vpc_id" , null)
   component = each.value.component
@@ -100,6 +100,6 @@ module "apps" {
   bastion_cidr = var.bastion_cidr
 }
 
-output "redis" {
-  value = module.elasticache
+output "alb" {
+  value = module.alb
 }
