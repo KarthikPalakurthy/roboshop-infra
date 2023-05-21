3fe37b7763fa8490815dc632ea4995c8cb5cd1ba
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-05-21 10:50:11 +0100

    terraform

--- main.tf
+++ main.tf
@@ -89,6 +89,7 @@ module "apps" {
   subnet_ids = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name , null), each.value.subnet_type, null), each.value.subnet_name, null), "subnet_ids", null )
   alb = lookup(lookup(module.alb,each.value.alb,null ), "dns_name", null )
   allow_cidr_blocks = lookup(lookup(lookup(lookup(var.vpc , each.value.vpc_name , null), each.value.allow_cidr_subnets_type , null),each.value.allow_cidr_subnets_name, null), "cidr_block", null)
+  listener = lookup(lookup(module.alb,each.value.alb,null ), "listener", null )
   vpc_id = lookup(lookup(module.vpc , each.value.vpc_name , null), "vpc_id" , null)
   component = each.value.component
   port_number = each.value.port_number
