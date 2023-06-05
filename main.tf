be362f5cacb2e90bc3511b71434d6174c578f6d5
KarthikPalakurthy <karthik.palakurthy95@gmail.com>
2023-06-05 13:46:16 +0100

    terraform

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ module "minikube" {
   aws_instance_type = "t2.medium"
   ssh_public_key = "~/.ssh/id_rsa.pub"
   aws_subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main" , null), "public_subnets", null), "public", null), "subnet_ids", null ), 0)
-  ami_image_id = data.aws_ami.centos8.id
+  //ami_image_id = data.aws_ami.centos8.id
   hosted_zone = var.hosted_zone
   hosted_zone_private = false
 
